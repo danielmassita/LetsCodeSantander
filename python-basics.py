8b3a44cc9433fce62e0eb214048755c8bb256681
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2022-09-13 01:26:33 -0300

    Update and rename 001.py to python-basics.py

---
+++ python-basics.py
@@ -0,0 +1,47 @@
+print("Hello, World!");
+
+"""Variável é um NOME e um VALOR que pode armazenar diversos tipos de dados..."""
+x = 5
+print(x)
+print(type(x))
+
+"""Os tipos de variáveis podem ser: INT (valores inteiros), FLOAT (valores decimais), STR (string) entre "aspas" e será uma cadeia de caracteres, BOOL (boolean """
+preco = 19.99
+print(preco, type(preco))
+cidade = "São Paulo"
+print(cidade, type(cidade))
+disponivel = True 
+print(disponivel)
+disponivel = False
+print(disponivel, type(disponivel))
+
+"""Os tipos de operadores serão Aritméticos e Lógicos. Os aritméticos são + - * /, resto da divisão %, divisão inteira // e potência **. 
+Os lógicos são not, and e or, para compor o booleano True/False."""
+x = 50
+y = 2
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x ** y)
+print(x // y)
+print(x % y)
+print(3/4)
+print(3//4)
+
+tem_cafe = True
+tem_pao = False
+print(not tem_cafe)
+print(tem_cafe or tem_pao)
+print(tem_cafe and tem_pao)
+
+"""Operadores Relacionais: maior que >, menor que <, igual a ==, maior ou igual >=, menor ou igual <=, diferente != serão usados pra retornar um True/False"""
+dolar = 5.3
+real = 1
+print(dolar > real)
+print(dolar < real)
+print(dolar == real)
+print(dolar >= real)
+print(dolar <= real)
+print(dolar != real)
+
