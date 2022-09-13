9bb74f9b66b7998ad2b48a2ca3f08302c6fd9d8c
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2022-09-13 02:59:49 -0300

    Create 03-python-estruturas-repeticao.py

---
+++ 03-python-estruturas-repeticao.py
@@ -0,0 +1,28 @@
+contador = 0
+while contador < 10:
+    contador = contador + 1
+    if contador == 1:
+        print(contador, "item limpo")
+    else: 
+        print(contador, "itens limpos")
+        
+print("Fim da repetição do bloco while.")
+
+contador = 0
+while True:
+    if contador < 10:
+        contador = contador + 1
+        if contador == 1:
+            print(contador, "item limpo")
+        else: 
+            print(contador, "itens limpos")
+    else:
+        break 
+        
+print("Fim da repetição do bloco while.")
+
+texto = input("Digita a sua senha: ")
+
+while texto != "LetsCode":
+    texto = input("Senha inválida, tente novamente. Digite a sua senha: ")
+print("Senha correta!")
